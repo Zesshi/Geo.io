5700478dc0462ffdc3bd2f6d4d480301253c8959
Jenny Tobias <jennyto@ict-campus.net>
2022-06-02 10:29:21 +0200

    Guess the Flag Europe

---
+++ app/src/main/java/net/ictcampus/geoio/GuessTheFlagOceaniaActivity.java
@@ -0,0 +1,14 @@
+package net.ictcampus.geoio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GuessTheFlagOceaniaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guess_the_flag_oceania);
+    }
+}
